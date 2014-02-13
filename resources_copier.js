25f840c5c78025a77c12a896239df99835fd8d36
Abdessamad Idrissi <numediaweb@yahoo.fr>
2014-02-13 17:44:27 +0100

    Update resources_copier.js
    Added support for AndroidManifest.xml; Copy the AndroidManifest.xml and prevent losing changes when you delete/rebuild the Android platform.

--- resources_copier.js
+++ resources_copier.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 /*
-// A cordova hook that copies various resource files, mainly icons and splash screens, into the appropriate platform specific location
+// A cordova hook that copies various resource files, mainly icons and splash screens, 
+// into the appropriate platform specific location.
+// Version; 1.1
+// https://github.com/numediaweb/resources_copier
 */
 
 // give the console some vibrant colors :)
@@ -18,7 +21,14 @@ var rootDir = process.cwd();
 
 //copies directory and subdirectories
 fs.copy(rootDir + '/config/android/res', rootDir + '/platforms/android/res', function (err) {
-    if (err) return console.error(err.red + ' An error occured! did you put your assets in; "config/android/res"?'.red);
+    if (err) return console.error(err.red + ' An error occured! did you put your assets in; "config/android/res"? \r\n'.red);
 
-    process.stdout.write('Resources successfully copied!'.green)
+    process.stdout.write('Resources successfully copied! \r\n'.green)
+});
+
+//Copy the AndroidManifest.xml and prevent losing changes when you delete/rebuild the platform
+fs.copy(rootDir + '/config/android/AndroidManifest.xml', rootDir + '/platforms/android/AndroidManifest.xml', function (err) {
+    if (err) return console.error(err.red + ' An error occured! Could not copy "AndroidManifest.xml" \r\n'.red);
+
+    process.stdout.write('AndroidManifest.xml successfully copied! \r\n'.green)
 });
